9e99ecaf853b034f3df8953b878f8473eaac82ef
stephen kurtz <stephenjkurtz@gmail.com>
2025-10-22 15:47:45 -0400

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,54 @@
 fn main() {
-    // Mutable variable example
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    // This line parses the string "42" into a u32 integer.
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("The u32 number is: {}", guess);
 
-    // Constant example
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    println!("Three hours in seconds: {THREE_HOURS_IN_SECONDS}");
+    // Integer types show allocated in memory
+    let a: u8 = 255; // 8-bit unsigned integer
+    let b: i8 = -128; // 8-bit signed integer
+    let c: u16 = 65_535; // 16-bit unsigned integer
+    let d: i16 = -32_768; // 16-bit signed integer
+    let e: u32 = 4_294_967_295; // 32-bit unsigned integer
+    let f: i32 = -2_147_483_648; // 32-bit signed integer
+    let g: u64 = 18_446_744_073_709_551_615; // 64-bit unsigned integer
+    let h: i64 = -9_223_372_036_854_775_808; // 64-bit signed integer
+    let i: u128 = 340_282_366_920_938_463_463_374_607_431_768_211_455; // 128-bit unsigned integer
+    let j: i128 = -170_141_183_460_469_231_731_687_303_715_884_105_728; // 128-bit signed integer
+    let k: isize = -9_223_372_036_854_775_808; // Pointer-sized signed integer
+    let l: usize = 18_446_744_073_709_551_615; // Pointer-sized unsigned integer    
+    println!(
+        "Integers: a: {}, b: {}, c: {}, d: {}, e: {}, f: {}, g: {}, h: {}, i: {}, j: {}, k: {}, l: {}",
+        a, b, c, d, e, f, g, h, i, j, k, l
+    );  
 
-    // Shadowing example
-    let y = 5;
+    // Floating point types
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
 
-    let y = y + 1;
+    println!("Floating point x: {}, Floating point y: {}", x, y);
 
-    {
-        let y = y * 2;
-        println!("The value of y in the inner scope is: {y}");
-    }
+    // Numeric operations
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3;
+    let remainder = 43 % 5;
 
-    println!("The value of y is: {y}");
+    println!(
+        "Sum: {}, Difference: {}, Product: {}, Quotient: {}, Truncated: {}, Remainder: {}",
+        sum, difference, product, quotient, truncated, remainder
+    );
 
-    // Shadowing with different types
-    let spaces = "   ";
-    let spaces = spaces.len();
-    println!("The number of spaces is: {spaces}");
+    // Boolean types
+    let t = true;
+    let f: bool = false;
 
-    let mut z = 10;
-    println!("The value of z is: {z}");
-    // z = "20";
-    println!("The value of z is: {z}");
+    println!("Boolean t: {}, Boolean f: {}", t, f);
+
+    // Character types
+    let c = 'z';
+    let z: char = 'ℤ';
+    let heart_eyed_cat = '😻';
+    println!("Characters: {}, {}, {}", c, z, heart_eyed_cat);
 }
